54b35dc57ad55ca82cc39754da975e9cd7e13dc0
damir.novosel <damir.novosel.hr@gmail.com>
2021-09-06 15:49:06 -0700

    Updated script

--- build-run-docker.sh
+++ build-run-docker.sh
@@ -2,7 +2,8 @@ mvn verify && docker build -t url-shortener .
 
 docker tag url-shortener:latest damirnovosel/url-shortener:1.0.0
 
-docker push damirnovosel/url-shortener:1.0.0
+sudo docker login 
+sudo docker push damirnovosel/url-shortener:1.0.0
 
 
 docker run -it -p 8080:8080 url-shortener:1.0.0
@@ -12,6 +13,11 @@ docker run -it -p 8080:8080 url-shortener:1.0.0
 
 docker pull damirnovosel/url-shortener:1.0.0
 
-docker images | grep fresh
+docker images | grep url-shortener
 
 docker run -it -p 8080:8080 damirnovosel/url-shortener:1.0.0
+
+
+curl localhost:8080/1 -v
+curl -X POST localhost:8080/shorten -H "Content-Type: text/plain" -d "http://google.com" -v
+curl localhost:8080/2 -v
